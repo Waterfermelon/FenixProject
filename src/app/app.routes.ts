689bea17a0141c1609df726a25179ed3d51215c7
Luis Rojas <rlluis109@gmail.com>
2025-03-02 12:48:09 -0600

    inicio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./views/landing/landing.component').then(m => m.LandingComponent),
+  }
+];
